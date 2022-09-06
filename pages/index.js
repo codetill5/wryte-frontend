f623e29bfbc0725a1408d6ed3a715685612cb439
codetill5 <mehrashubham216@gmail.com>
2022-09-06 18:17:48 +0530

    added: header

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import HeadTitle from "../components/header/HeadTitle";
 import FooterOne from "../components/footer";
+import HeaderOne from "../components/header";
 
-const HomeDefault = ({ allPosts }) => {
+const HomeDefault = () => {
   return (
     <>
       <HeadTitle pageTitle="Home Default" />
+      <HeaderOne />
       <FooterOne />
     </>
   );
