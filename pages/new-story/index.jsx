f97f2af8af694e34561bd5dd4f1d2bb685e0eba6
codetill5 <mehrashubham216@gmail.com>
2022-09-12 19:32:40 +0530

    added: new story ui

---
+++ pages/new-story/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const NewStory = () => {
+  return (
+    <div className="axil-contact-form-area storyContainer">
+        <div className='floatImgContainer'>
+      <div className='leftImgContainer'> 
+        <img src='/assets/3d-assets/3dTriangle.png' alt='3dObjects' className='threeObjects' />
+        <img src='/assets/3d-assets/ball.png' alt='3dObjects' className='threeObjects' />
+        <img src='/assets/3d-assets/circle.png' alt='3dObjects' className='threeObjects' />
+        </div>
+
+
+        <div className='storyBlock'>
+        </div>
+
+        <div className='rightImgContainer'>
+        <img src='/assets/3d-assets/cube.png' alt='3dObjects' className='threeObjects' />
+        <img src='/assets/3d-assets/hexagon.png' alt='3dObjects' className='threeObjects' />
+        <img src='/assets/3d-assets/triangle.png' alt='3dObjects' className='threeObjects' />
+        </div>
+
+        </div>
+
+        {/* <div className='storyBlock'>
+hey
+        </div> */}
+
+    </div>
+  )
+}
+
+export default NewStory
