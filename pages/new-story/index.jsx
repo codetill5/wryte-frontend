38fc9f6bb7e4c7ffb2c0c37e8cff456157cc73eb
codetill5 <mehrashubham216@gmail.com>
2022-09-13 11:29:20 +0530

    feat: editor

--- pages/new-story/index.jsx
+++ pages/new-story/index.jsx
@@ -1,33 +1,72 @@
-import React from 'react'
+import React from "react";
+import dynamic from "next/dynamic";
+// import { createReactEditorJS } from "react-editor-js";
+
+
+
+
+
+// const tools = dynamic(() => import("../../components/Editor/constants"), { ssr: false });
+
+
+// import { EDITOR_JS_TOOLS } from "../../components/Editor/constants";
 
 const NewStory = () => {
-  return (
-    <div className="axil-contact-form-area storyContainer">
-        <div className='floatImgContainer'>
-      <div className='leftImgContainer'> 
-        <img src='/assets/3d-assets/3dTriangle.png' alt='3dObjects' className='threeObjects' />
-        <img src='/assets/3d-assets/ball.png' alt='3dObjects' className='threeObjects' />
-        <img src='/assets/3d-assets/circle.png' alt='3dObjects' className='threeObjects' />
-        </div>
+  // const ReactEditorJS = createReactEditorJS();
 
-
-        <div className='storyBlock'>
-        </div>
-
-        <div className='rightImgContainer'>
-        <img src='/assets/3d-assets/cube.png' alt='3dObjects' className='threeObjects' />
-        <img src='/assets/3d-assets/hexagon.png' alt='3dObjects' className='threeObjects' />
-        <img src='/assets/3d-assets/triangle.png' alt='3dObjects' className='threeObjects' />
-        </div>
-
-        </div>
-
-        {/* <div className='storyBlock'>
-hey
-        </div> */}
-
-    </div>
-  )
+  let MyEditor;
+if (typeof window !== "undefined") {
+  MyEditor = dynamic(() => import('../../components/Editor'));
 }
 
-export default NewStory
+  return (
+   
+      <MyEditor />
+  
+    // <div className="axil-contact-form-area storyContainer">
+      /* <div className="floatImgContainer">
+        <div className="leftImgContainer">
+          <img
+            src="/assets/3d-assets/3dTriangle.png"
+            alt="3dObjects"
+            className="threeObjects"
+          />
+          <img
+            src="/assets/3d-assets/ball.png"
+            alt="3dObjects"
+            className="threeObjects"
+          />
+          <img
+            src="/assets/3d-assets/circle.png"
+            alt="3dObjects"
+            className="threeObjects"
+          />
+        </div>
+
+        <div className="storyBlock"> */
+         
+        /* </div>
+
+        <div className="rightImgContainer">
+          <img
+            src="/assets/3d-assets/cube.png"
+            alt="3dObjects"
+            className="threeObjects"
+          />
+          <img
+            src="/assets/3d-assets/hexagon.png"
+            alt="3dObjects"
+            className="threeObjects"
+          />
+          <img
+            src="/assets/3d-assets/triangle.png"
+            alt="3dObjects"
+            className="threeObjects"
+          />
+        </div>
+      </div> */
+    /* </div> */
+  );
+};
+
+export default NewStory;
