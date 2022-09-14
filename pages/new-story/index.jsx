adc2c6e2b88b50caf5dca749c8bd9c15fb8b01ff
codetill5 <mehrashubham216@gmail.com>
2022-09-14 15:04:59 +0530

    fix

--- pages/new-story/index.jsx
+++ pages/new-story/index.jsx
@@ -1,30 +1,9 @@
 import React from "react";
-import dynamic from "next/dynamic";
-// import { createReactEditorJS } from "react-editor-js";
-
-
-
-
-
-// const tools = dynamic(() => import("../../components/Editor/constants"), { ssr: false });
-
-
-// import { EDITOR_JS_TOOLS } from "../../components/Editor/constants";
 
 const NewStory = () => {
-  // const ReactEditorJS = createReactEditorJS();
-
-  let MyEditor;
-if (typeof window !== "undefined") {
-  MyEditor = dynamic(() => import('../../components/Editor'));
-}
-
   return (
-   
-      <MyEditor />
-  
-    // <div className="axil-contact-form-area storyContainer">
-      /* <div className="floatImgContainer">
+    <div className="axil-contact-form-area storyContainer">
+      <div className="floatImgContainer">
         <div className="leftImgContainer">
           <img
             src="/assets/3d-assets/3dTriangle.png"
@@ -43,9 +22,7 @@ if (typeof window !== "undefined") {
           />
         </div>
 
-        <div className="storyBlock"> */
-         
-        /* </div>
+        <div className="storyBlock"></div>
 
         <div className="rightImgContainer">
           <img
@@ -64,8 +41,8 @@ if (typeof window !== "undefined") {
             className="threeObjects"
           />
         </div>
-      </div> */
-    /* </div> */
+      </div>
+    </div>
   );
 };
 
