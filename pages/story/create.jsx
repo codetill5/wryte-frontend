4c5f4ea72288f02e9a5d7164710be415fdcb4ad7
codetill5 <mehrashubham216@gmail.com>
2022-09-15 13:23:13 +0530

    story editor

---
+++ pages/story/create.jsx
@@ -0,0 +1,26 @@
+import Editor from "../../components/Editor/Editor";
+
+const Create = (props) => {
+  const onSaveHandler = async (blogData, title, description) => {
+    const toSaveData = {
+      title,
+      blogData,
+      description,
+    };
+
+    console.log(toSaveData);
+  };
+
+  return (
+    <div className="storyContainer">
+     
+      <Editor
+        onSave={(editorData, title, description) =>
+          onSaveHandler(editorData, title, description)
+        }
+      />
+    </div>
+  );
+};
+
+export default Create;
